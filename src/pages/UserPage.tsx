dc0f793d019894fc42a834a318a51e2402a804c7
rahulsan007 <santrarahul7070@gmail.com>
2024-06-04 15:09:08 +0530

    state value update

--- src/pages/UserPage.tsx
+++ src/pages/UserPage.tsx
@@ -8,7 +8,7 @@ import SavedItems from "@/components/SavedItems";
 function UserPage() {
   return (
     <div className="">
-      <Tabs defaultValue="account" className="w-full">
+      <Tabs defaultValue="overview" className="w-full">
         <TabsList className="">
           <TabsTrigger asChild value="overview">
             <button className=" text-primary1 bg-primary2 px-4 py-2">
